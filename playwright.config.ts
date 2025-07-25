d21f984b55480f1460836898839e284f5d433f90
Sigurður Ellert Sigurjónsson <hr.sellert@gmail.com>
2025-07-25 11:03:31 +0200

    Cleaning up reports a bit

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  reporter: [['html', { outputFolder: 'playwright-report', open: 'on-failure' }]],
+  reporter: [['html', { outputFolder: 'test-results', open: 'on-failure' }]],
   use: {
     screenshot: 'on',
     video: 'retain-on-failure',
