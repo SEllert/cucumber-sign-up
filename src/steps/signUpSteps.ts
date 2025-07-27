9193c216d72278387cf284ae87419cbf3c0ce450
Sigurður Ellert Sigurjónsson <hr.sellert@gmail.com>
2025-07-27 15:38:22 +0200

    Adding full page screen shots

--- src/steps/signUpSteps.ts
+++ src/steps/signUpSteps.ts
@@ -4,14 +4,12 @@ import { page } from '../../browswerSetup';
 import { SignUpPage } from '../page-objects/signup-page.pom';
 import { CustomWorld } from '../support/world';
 
-// No more global variable! 🎉
-// let signUpPage: SignUpPage;  // ❌ Removed global state
 
 Given('the user is on the signup page', async function (this: CustomWorld) {
     // 'this' refers to the CustomWorld instance for this scenario
     this.signUpPage = new SignUpPage(page);
     await this.signUpPage.goto();
-    const screenshot = await page.screenshot();
+    const screenshot = await this.signUpPage.page.screenshot();
     await this.attach(screenshot, 'image/png');
 });
 
@@ -40,8 +38,11 @@ When('the user presses the Enter key', async () => {
 }); 
 
 Then('the user should see their name {string} on the petition page', async function (this: CustomWorld, fullName: string) {
-    const html = await page.content();
-    expect(html).toContain(fullName);
-    const screenshot = await page.screenshot();
+    // Find the container div
+    const container = page.locator('div.max-h-96.overflow-y-auto');
+    // Search for the name only within the container
+    const nameLocator = container.locator(`span.text-gray-900.font-medium.text-lg:has-text("${fullName}")`);
+    await expect(nameLocator).toBeVisible();
+    const screenshot = await page.screenshot({ fullPage: true });
     await this.attach(screenshot, 'image/png');
 });
