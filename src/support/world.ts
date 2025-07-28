ded98b67842ea49580c52c2cc04638018d65b2ff
Sigurður Ellert Sigurjónsson <hr.sellert@gmail.com>
2025-07-28 17:05:08 +0200

    Adding some more browsers

--- src/support/world.ts
+++ src/support/world.ts
@@ -1,5 +1,5 @@
 import { setWorldConstructor, World } from '@cucumber/cucumber';
-import { Browser, Page, chromium } from 'playwright';
+import { Browser, Page, chromium, firefox, webkit, BrowserType } from 'playwright';
 import type { SignUpPage } from '../page-objects/signup-page.pom';
 
 export class CustomWorld extends World {
@@ -10,8 +10,26 @@ export class CustomWorld extends World {
   signUpPage?: import('../page-objects/signup-page.pom').SignUpPage;
 
   async initBrowser() {
-    console.log('initBrowser called')
-    this.browser = await chromium.launch();
+    // Get browser from environment variable or default to chromium
+    const browserName = process.env.BROWSER?.toLowerCase() || 'chromium';
+    
+    // Select browser based on name
+    let browserType: BrowserType;
+    switch (browserName) {
+      case 'firefox':
+        browserType = firefox;
+        console.log('Using Firefox browser');
+        break;
+      case 'webkit':
+        browserType = webkit;
+        console.log('Using WebKit browser');
+        break;
+      default:
+        browserType = chromium;
+        console.log('Using Chromium browser');
+    }
+    
+    this.browser = await browserType.launch();
     this.page = await this.browser.newPage();
   }
 
